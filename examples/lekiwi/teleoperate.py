9d687d0ae3e52f5365dd3291a0214ff364606b23
loewenherz-projekt <5024739+loewenherz-projekt@users.noreply.github.com>
2025-07-04 14:37:51 +0200

    Update teleoperate.py

--- examples/lekiwi/teleoperate.py
+++ examples/lekiwi/teleoperate.py
@@ -9,7 +9,7 @@ from lerobot.utils.visualization_utils import _init_rerun, log_rerun_data
 FPS = 30
 
 # Create the robot and teleoperator configurations
-robot_config = LeKiwiClientConfig(remote_ip="172.18.134.136", id="my_lekiwi")
+robot_config = LeKiwiClientConfig(remote_ip="192.168.178.111", id="my_lekiwi")
 teleop_arm_config = SO100LeaderConfig(port="/dev/tty.usbmodem585A0077581", id="my_awesome_leader_arm")
 keyboard_config = KeyboardTeleopConfig(id="my_laptop_keyboard")
 
