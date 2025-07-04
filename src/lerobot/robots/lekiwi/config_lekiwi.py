d4ab69f2b0c0d956e14bfd112aaaa6b6ed4fa693
loewenherz-projekt <5024739+loewenherz-projekt@users.noreply.github.com>
2025-07-04 14:25:27 +0200

    Update config_lekiwi.py

--- src/lerobot/robots/lekiwi/config_lekiwi.py
+++ src/lerobot/robots/lekiwi/config_lekiwi.py
@@ -26,7 +26,7 @@ def lekiwi_cameras_config() -> dict[str, CameraConfig]:
             index_or_path="/dev/video0", fps=30, width=640, height=240, rotation=Cv2Rotation.ROTATE_180
         ),
         "wrist": OpenCVCameraConfig(
-            index_or_path="/dev/video2", fps=30, width=640, height=240, rotation=Cv2Rotation.ROTATE_90
+            index_or_path="/dev/video2", fps=30, width=640, height=240, rotation=Cv2Rotation.ROTATE_0
         ),
     }
 
